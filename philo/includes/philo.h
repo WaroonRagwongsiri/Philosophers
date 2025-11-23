59480a29cf5169dc77134ebd4255c00d7ad76d05
WaroonRagwongsiri <waroonwork@gmail.com>
2025-11-23 16:20:31 +0700

    tries thread creating

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,11 +6,41 @@
 /*   By: waroonwork@gmail.com <WaroonRagwongsiri    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 14:13:08 by waroonwork@       #+#    #+#             */
-/*   Updated: 2025/11/23 14:13:21 by waroonwork@      ###   ########.fr       */
+/*   Updated: 2025/11/23 16:16:20 by waroonwork@      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_philo
+{
+	int	index;
+	int	t_die;
+	int	t_eat;
+	int	t_sleep;
+	int	eat_count;
+}	t_philo;
+
+typedef struct s_table
+{
+	int			n_philo;
+	int			t_die;
+	int			t_eat;
+	int			t_sleep;
+	int			n_eat_end;
+	t_philo		*philo_arr;
+	pthread_t	*thread;
+}	t_table;
+
+// Utils
+long	ft_atol(const char *nptr);
 
 #endif
---
+++ philo/srcs/utils/utils1.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils1.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waroonwork@gmail.com <WaroonRagwongsiri    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/23 15:47:44 by waroonwork@       #+#    #+#             */
+/*   Updated: 2025/11/23 15:48:27 by waroonwork@      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long	ft_atol(const char *nptr)
+{
+	long	ans;
+	int		i;
+	int		neg;
+
+	if (!nptr)
+		return (0);
+	i = 0;
+	ans = 0;
+	neg = 1;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		ans = ans * 10 + nptr[i] - '0';
+		i++;
+	}
+	return (ans * neg);
+}
